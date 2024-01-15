2b2a6beb6aa43f91496ba0df35ca15e88c27ce3c
Joseph Gaughan <jojopotato9393@gmail.com>
2024-01-15 11:35:31 -0600

    exercise 1-2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            
+            
+
+            int sum = a + b;
+            Console.WriteLine($"{a}+{b} is {sum}");
+            Console.WriteLine();
+
+            int product = a * b;
+            Console.WriteLine($"{a}*{b} is {product}");
+            Console.WriteLine();
+
+            int difference = a - b;
+            Console.WriteLine($"{a}-{b} is {difference}");
+            Console.WriteLine();
+
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            Console.WriteLine();
+
+            
+            Console.WriteLine("what is the radius of your circle");
+            double radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"Area of the circle with a radius of {radius} is {area}");
+            //this still reads .. not sure now to make it not..
+        }
+        static double AreaOfCircle(double radius)
+        {
+            if (radius >= 0)
+            {
+                double area = Math.PI * Math.Pow(radius, 2);
+                return area;
+            }
+            else
+             {
+                Console.WriteLine("invalid input");
+                return 0;
+
+             }
+
         }
     }
 }
+
+// and thought exercise = 16
